c7ca68151d61feb1df0a3aca584e20894615347c
roshani226 <86221628+roshani226@users.noreply.github.com>
2022-05-08 08:01:27 +0530

    commit
    server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
+const path = require('path')
+const { ppid } = require('process')
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
