e30819b5e78f64de1c5ab05d8295c77ca9c06524
roshani226 <86221628+roshani226@users.noreply.github.com>
2022-05-08 08:01:57 +0530

    Update server.js
    server.js updated

--- server.js
+++ server.js
@@ -15,4 +15,64 @@ app.use(cookieParser())
 app.use(fileUpload({
     useTempFiles: true
 }))
+// Routes
+app.use('/user', require('./routes/userRouter'))
+app.use('/api', require('./routes/upload'))
+const orderRouter = require("./routes/orders");
+app.use("/order",orderRouter);
 
+const supplierRouter = require("./routes/supplier");
+
+app.use("/supplier",supplierRouter);
+
+const deliveryRouter = require("./routes/delivery");
+
+app.use("/delivery",deliveryRouter);
+
+const returnstockRouter = require("./routes/returnstock");
+
+app.use("/returnstock",returnstockRouter);
+
+const branchRouter = require("./routes/branch");
+
+app.use("/branch",branchRouter);
+
+const itemRouter = require("./routes/item");
+
+app.use("/item",itemRouter);
+
+const employeeRouter = require("./routes/employee");
+
+ app.use("/employee",employeeRouter);
+
+ const financesRouter = require("./routes/finances");
+
+ app.use("/finances",financesRouter);
+
+
+
+// Connect to mongodb
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if(err) throw err;
+    console.log("Connected to mongodb")
+})
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT)
+})
