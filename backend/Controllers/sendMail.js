ae892e7a871076d108021516c19f68b6e45fde7d
roshani226 <86221628+roshani226@users.noreply.github.com>
2022-05-08 11:33:04 +0530

    sendmail
    sendmail.js

---
+++ backend/Controllers/sendMail.js
@@ -0,0 +1,12 @@
+const nodemailer = require('nodemailer')
+const {google} = require('googleapis')
+const {OAuth2} = google.auth;
+const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground'
+
+const {
+    MAILING_SERVICE_CLIENT_ID,
+    MAILING_SERVICE_CLIENT_SECRET,
+    MAILING_SERVICE_REFRESH_TOKEN,
+    SENDER_EMAIL_ADDRESS
+} = process.env
+
