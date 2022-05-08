53fac6d42c3ca0ef9edccd2d8b22318e1088ed31
roshani226 <86221628+roshani226@users.noreply.github.com>
2022-05-08 11:33:37 +0530

    Update sendMail.js
    sendmail.js

--- backend/Controllers/sendMail.js
+++ backend/Controllers/sendMail.js
@@ -10,3 +10,28 @@ const {
     SENDER_EMAIL_ADDRESS
 } = process.env
 
+const oauth2Client = new OAuth2(
+    MAILING_SERVICE_CLIENT_ID,
+    MAILING_SERVICE_CLIENT_SECRET,
+    MAILING_SERVICE_REFRESH_TOKEN,
+    OAUTH_PLAYGROUND
+)
+
+// send mail
+const sendEmail = (to, url, txt) => {
+    oauth2Client.setCredentials({
+        refresh_token: MAILING_SERVICE_REFRESH_TOKEN
+    })
+
+    const accessToken = oauth2Client.getAccessToken()
+    const smtpTransport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            type: 'OAuth2',
+            user: SENDER_EMAIL_ADDRESS,
+            clientId: MAILING_SERVICE_CLIENT_ID,
+            clientSecret: MAILING_SERVICE_CLIENT_SECRET,
+            refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
+            accessToken
+        }
+    })
